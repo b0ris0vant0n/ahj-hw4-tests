ff07c58de5961962d15c1cda804d816767d213a9
Anton Borisov <borisov2011@gmail.com>
2023-07-25 10:57:27 +0300

    commit

--- src/js/__tests__/validating.test.js
+++ src/js/__tests__/validating.test.js
@@ -1,16 +1,16 @@
 import validate from '../validating'
 
 test.each( [
-  ['Valid', '4276550037212597', true],
-  ['Not valid 1','2232134345', false],
-  ['Not valid 2','42256', false],
-  ['Not valid 3','4276550037212598', false],
-  ['Not valid 4','9999999999999999', false],
+    ['Valid', '4276550037212597', true],
+    ['Not valid 1' ,'2232134345', false],
+    ['Not valid 2' ,'42256', false],
+    ['Not valid 3' ,'4276550037212598', false],
+    ['Not valid 4' ,'9999999999999999', false],
 
-])(
-  ('check the valid of credit card, should be %s'), 
-  (_, value, expected) => {
-    const result = validate(value)
+  ])(
+    ('check the valid of CreditCard, should be %s'), 
+    (_, value, expected) => {
+      const result = validate(value)
 
     expect(result).toBe(expected)
-});
+  })
